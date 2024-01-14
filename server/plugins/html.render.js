ac0a34645ae6d513e76c12df0dd5d2959b9568a4
droplister <anderson@familymedia.org>
2024-01-14 04:21:30 -0500

    Example UI

---
+++ server/plugins/html.render.js
@@ -0,0 +1,8 @@
+import { defineNitroPlugin } from 'nitropack/runtime/plugin'
+
+export default defineNitroPlugin((nitroApp) => {
+  nitroApp.hooks.hook('render:html', (html, { event }) => {
+      html.htmlAttrs.push(" class='h-full bg-gray-900'")
+      html.bodyAttrs.push(" class='h-full'")
+  })
+})
