1d23fee9db2d00fefd69d2d9d1882c3e4c3023dc
droplister <anderson@familymedia.org>
2024-02-12 01:03:51 -0500

    Update [collection].vue

--- pages/collection/[collection].vue
+++ pages/collection/[collection].vue
@@ -2,7 +2,7 @@
   <div class="lg:flex lg:items-center lg:justify-between">
     <div class="min-w-0 flex-1">
       <h1 class="text-2xl font-bold leading-7 text-white sm:truncate sm:text-3xl sm:tracking-tight">
-        Collection
+        {{ apiData.name }}
       </h1>
       <div class="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
       </div>
@@ -39,6 +39,7 @@ import { ref, watchEffect } from 'vue';
 
 const route = useRoute();
 const collection = ref(route.params.collection);
+const apiData = ref({ name: '', link: '', stamp: 0, assets_count: 0 });
 
 const tabs = [
   { name: 'Activity' },
@@ -53,8 +54,26 @@ const handleTabChange = (selectedTab) => {
   activeTab.value = selectedTab;
 };
 
+const fetchData = async () => {
+  try {
+    const response = await fetch(`https://api.xcp.io/api/tags?slug=${collection.value}`);
+    if (!response.ok) throw new Error('Network response was not ok');
+    const data = await response.json();
+    apiData.value.name = data[0].name;
+    apiData.value.link = data[0].link;
+    apiData.value.stamp = data[0].stamp;
+    apiData.value.assets_count = data[0].assets_count;
+  } catch (error) {
+    console.error('Fetch error:', error);
+  }
+};
+
 watchEffect(() => {
   collection.value = route.params.collection;
+});
+
+onMounted(() => {
+  fetchData();
 });
 
 </script>
