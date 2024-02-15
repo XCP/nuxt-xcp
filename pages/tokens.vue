883ef3458d710e0bd8a74973ec453e387330d1f2
droplister <anderson@familymedia.org>
2024-02-15 04:04:52 -0500

    Update tokens.vue

--- pages/tokens.vue
+++ pages/tokens.vue
@@ -2,7 +2,7 @@
   <div class="lg:flex lg:items-center lg:justify-between">
     <div class="min-w-0 flex-1">
       <h1 class="text-2xl font-bold leading-7 text-white sm:truncate sm:text-3xl sm:tracking-tight">
-        Cryptocurrency
+        Cryptocurrencies
       </h1>
     </div>
   </div>
