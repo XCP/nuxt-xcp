9229e5b13df92b23fe51d93c2b7c1a7d13b01308
droplister <anderson@familymedia.org>
2024-07-08 00:53:53 -0400

    No Upstash

--- server/plugins/html.render.js
+++ server/plugins/html.render.js
@@ -2,7 +2,7 @@ import { defineNitroPlugin } from 'nitropack/runtime/plugin'
 
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('render:html', (html, { event }) => {
-      html.htmlAttrs.push(" class='h-full'")
+      html.htmlAttrs.push(" class='h-full bg-gray-900'")
       html.bodyAttrs.push(" class='h-full'")
   })
 })
