8a9a44c4f3aaedbf1ba8db153318406d3b0d58e9
droplister <anderson@familymedia.org>
2024-07-14 21:27:06 -0400

    Update prices.js

--- server/api/prices.js
+++ server/api/prices.js
@@ -1,6 +1,6 @@
+// server/api/prices.js
 import { defineEventHandler, getQuery } from 'h3'
 import CoinpaprikaAPI from '@coinpaprika/api-nodejs-client'
-import { getCachedData } from '~/utils/getCachedData'
 
 const client = new CoinpaprikaAPI({
   baseURL: 'https://api-pro.coinpaprika.com',
@@ -10,21 +10,12 @@ export default defineEventHandler(async (event) => {
   const { coinId } = getQuery(event)
   const apiKey = process.env.COINPAPRIKA_API_KEY
 
-  console.log(process.env.COINPAPRIKA_API_KEY)
-
   try {
-    const ticker = await getCachedData(
-      `price_${coinId}`,
-      async () => {
-        const response = await client.getTicker({ coinId, headers: { Authorization: apiKey } })
-        console.log(response)
-        return response
-      },
-      30, // Cache TTL in seconds
-    )
-    return ticker
-  }
-  catch (error) {
+    const response = await client.getTicker({ coinId, headers: { Authorization: apiKey } })
+    console.log('API Response:', response)
+    return response
+  } catch (error) {
+    console.error('API Error:', error)
     return { error: error.message }
   }
 })
